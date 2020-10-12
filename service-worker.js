6396ed18632e36e12feb5ab454112782c9d63494
irwanto dwi subagus <66657551+irwantods@users.noreply.github.com>
2020-10-12 11:49:22 +0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,56 @@
 const CACHE_NAME = "PWApertama-v0";
 var urlsToCache = [
     "/",
+    "/manifest.json",
     "/nav.html",
     "/index.html",
-
+    //page navigasi
     "/pages/home.html",
     "/pages/about.html",
     "/pages/contact.html",
     "/pages/gallery.html",
+    // css
     "/css/materialize.min.css",
+    "/css/materialize.css",
+    "/css/style.css",
+    "https://fonts.googleapis.com/icon?family=Material+Icons",
+    "/css/PatrickHand-Regular.ttf",
+    // logo
+    "./image/wedding_logo.png",
+    // aset image home
+    "/image/wed28.webp",
+    "/image/wed20.webp",
+    "/image/wash_hand.png",
+    "/image/mask.png",
+    "/image/distancing.png",
+    "/image/wed21.webp",
+    //aset image gallery
+    "/image/wed19.webp",
+    "/image/wed23.webp",
+    "/image/wed8.webp",
+    "/image/wed27.webp",
+    "/image/wed6.webp",
+    "/image/wed2.webp",
+    "/image/wed12.webp",
+    "/image/wed7.webp",
+    //file JS
+    "/js/materialize.js",
     "/js/materialize.min.js",
-    "/js/nav.js"
+    "/js/nav.js",
+    "js/sw-register.js",
+    //icon
+    "/image/icon_128.ico",
+    "./image/icon_32.png",
+    "./image/icon_64.png",
+    "./image/icon_128.png",
+    "./image/icon_144.png",
+    "./image/icon_152.png",
+    "./image/icon_192.png",
+    "./image/icon_256.png",
+    "./image/icon_384.png",
+    "./image/icon_512.png"
+
+
 
 ];
 
